a8d5af7f353a00f5335e1919f97885f99707d9e2
Orlando Castillo <ocastillo@saatvamattress.com>
2018-03-30 13:36:51 -0500

    Finishing contact form

--- app/client/src/js/components/contactForm.jsx
+++ app/client/src/js/components/contactForm.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import {render} from 'react-dom'
 import fetch from 'isomorphic-fetch'
+import Recaptcha from 'react-google-recaptcha'
 
 const initialState = {
   complete: false,
-  formData: {
-    contact_name: '',
-    contact_email: '',
-    contact_subject: '',
-    message: ''
-  },
-  errors: {
-    form: null,
-    contact_name: null,
-    contact_email: null,
-    contact_subject: null,
-    message: null
-  }
+  recaptchaValue: null,
+  formData_contact_name: '',
+  formData_contact_email: '',
+  formData_contact_subject: '',
+  formData_message: '',
+  errors_form: null,
+  errors_contact_name: null,
+  errors_contact_email: null,
+  errors_contact_subject: null,
+  errors_message: null
 }
 
 const FormError = props => {
@@ -36,47 +34,54 @@ class ContactForm extends React.Component {
   }
 
   clearFormError () {
-    const newState = Object.assign({}, this.state)
-    newState.errors.form = null
-    this.setState(newState)
+    this.setState((prevState, props) => ({
+      errors_form: null,
+      complete: false
+    }))
   }
 
   handleSuccess () {
     const newState = JSON.parse(JSON.stringify(initialState))
     newState.complete = true
+    grecaptcha.reset()
     this.setState(newState)
   }
 
   validateAll () {
-    this.validateEmail(this.state.formData.contact_email)
-    this.validateRequired('contact_subject', this.state.formData.contact_subject)
-    this.validateRequired('contact_name', this.state.formData.contact_name)
-    this.validateRequired('message', this.state.formData.message)
+    this.validateRecaptcha(this.state.recaptchaValue)
+    this.validateEmail(this.state.formData_contact_email)
+    this.validateRequired('contact_subject', this.state.formData_contact_subject)
+    this.validateRequired('contact_name', this.state.formData_contact_name)
+    this.validateRequired('message', this.state.formData_message)
+  }
+
+  validateRecaptcha (rValue) {
+    const formError = this.state.recaptchaValue === null ? 'Please verify that you are human' : null
+    this.setState((prevState, props) => ({
+      errors_form: formError
+    }))
   }
 
   validateEmail (email) {
     const re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-    const newState = Object.assign({}, this.state)
-    newState.errors['contact_email'] = null
-    if(!re.test(email)) {
-      newState.errors['contact_email'] = 'Please enter a valid email'
-    }
-    this.setState(newState)
+    const emailError = re.test(email) ? null : 'Please enter a valid email'
+    this.setState((prevState, props) => ({
+      errors_contact_email: emailError
+    }))
   }
 
   validateRequired (field, fieldData) {
-    const newState = Object.assign({}, this.state)
-    newState.errors[field] = null
-    if(!fieldData) {
-      newState.errors[field] = 'This field is required.'
-      this.setState(newState)
-    }
+    const fieldError = (fieldData.length) ? null : 'This field is required'
+    const newState = {}
+    newState['errors_' + field] = fieldError
+    this.setState((prevState, props) => newState)
   }
 
   handleFieldChange (event) {
-    const newState = Object.assign({}, this.state)
-    newState.formData[event.target.name] = event.target.value
-    this.setState(newState);
+    const newState = {}
+    newState['formData_' + event.target.name] = event.target.value
+    this.setState((prevState, props) => newState)
+
     if(event.target.name === 'contact_email') {
       this.validateEmail(event.target.value)
     } else {
@@ -90,11 +95,19 @@ class ContactForm extends React.Component {
     this.clearFormError()
     this.validateAll()
 
-    if(this.state.errors.contact_name
-      || this.state.errors.contact_email
-      || this.state.errors.contact_subject
-      || this.state.errors.message) {
+    if( !this.state.recaptchaValue
+      || this.state.errors_contact_name
+      || this.state.errors_contact_email
+      || this.state.errors_contact_subject
+      || this.state.errors_message) {
         return false
+    }
+
+    const postData = {
+      contact_name: this.state.formData_contact_name,
+      contact_email: this.state.formData_contact_email,
+      contact_subject: this.state.formData_contact_subject,
+      message: this.state.formData_message
     }
 
     const fetchOptions = {
@@ -102,7 +115,7 @@ class ContactForm extends React.Component {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(this.state.formData)
+      body: JSON.stringify(postData)
     }
 
     fetch('/contact', fetchOptions)
@@ -115,37 +128,52 @@ class ContactForm extends React.Component {
         this.handleSuccess()
       }).catch(err => {
         console.log(err)
-        const newState = Object.assign({}, this.state)
-        newState.errors.form = err.message
-        this.setState(newState)
+        this.setState((prevState, props) => ({
+          errors_form: err.message
+        }))
       })
+  }
+
+  recaptchaCallback (rValue) {
+    this.setState((prevState, props) => ({
+      recaptchaValue: rValue
+    }))
+    this.validateRecaptcha(rValue)
   }
 
   render () {
     return (
       <form onSubmit={ this.handleSubmit }>
-        {this.state.errors.form &&
-          <FormError>{ this.state.errors.form }</FormError>
+        {this.state.errors_form &&
+          <FormError>{ this.state.errors_form }</FormError>
         }
         {this.state.complete &&
           <FormSuccess />
         }
-        <input type="text" name="contact_name" value={ this.state.formData.contact_name } placeholder="Your Name" onChange={ this.handleFieldChange } />
-        {this.state.errors.contact_name &&
-          <FormError>{ this.state.errors.contact_name }</FormError>
+        <input type="text" name="contact_name" value={ this.state.formData_contact_name } placeholder="Your Name" onChange={ this.handleFieldChange } />
+        {this.state.errors_contact_name &&
+          <FormError>{ this.state.errors_contact_name }</FormError>
         }
-        <input type="email" name="contact_email" value={ this.state.formData.contact_email } placeholder="Email Address" onChange={ this.handleFieldChange } />
-        {this.state.errors.contact_email &&
-          <FormError>{ this.state.errors.contact_email }</FormError>
+        <input type="email" name="contact_email" value={ this.state.formData_contact_email } placeholder="Email Address" onChange={ this.handleFieldChange } />
+        {this.state.errors_contact_email &&
+          <FormError>{ this.state.errors_contact_email }</FormError>
         }
-        <input type="text" name="contact_subject" value={ this.state.formData.contact_subject } placeholder="Subject" onChange={ this.handleFieldChange } />
-        {this.state.errors.contact_subject &&
-          <FormError>{ this.state.errors.contact_subject }</FormError>
+        <input type="text" name="contact_subject" value={ this.state.formData_contact_subject } placeholder="Subject" onChange={ this.handleFieldChange } />
+        {this.state.errors_contact_subject &&
+          <FormError>{ this.state.errors_contact_subject }</FormError>
         }
-        <textarea name="message" value={ this.state.formData.message } placeholder="Type your Message" onChange={ this.handleFieldChange }></textarea>
-        {this.state.errors.message &&
-          <FormError>{ this.state.errors.message }</FormError>
+        <textarea name="message" value={ this.state.formData_message } placeholder="Type your Message" onChange={ this.handleFieldChange }></textarea>
+        {this.state.errors_message &&
+          <FormError>{ this.state.errors_message }</FormError>
         }
+        <div className="o-recaptcha">
+          <Recaptcha 
+            sitekey="6Ld-k08UAAAAAFVi19wMXcyNgnnsqpoXqZHpiOVS" 
+            ref="recaptcha" 
+            size="compact"
+            onChange={ rValue => this.recaptchaCallback(rValue) }
+          />
+          </div>
         <input type="submit" value="Send" />
       </form>
     )
